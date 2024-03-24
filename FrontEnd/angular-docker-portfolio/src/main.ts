c2ba5d180067fe0890abd2d9f31c49b39c1eb4a1
sholden3 <wesholden1992@gmail.com>
2024-03-24 15:32:04 -0400

    AB testing and Feature flags via posthog setup

--- FrontEnd/angular-docker-portfolio/src/main.ts
+++ FrontEnd/angular-docker-portfolio/src/main.ts
@@ -4,9 +4,18 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import posthog from 'posthog-js'
+
 if (environment.production) {
   enableProdMode();
 }
+
+posthog.init(
+  'phc_Yv2l8W8bYdHXzM6SToxoQ7MQsVP1z5vhyyR6YZF0KLo',
+  {
+    api_host:"https://us.posthog.com"
+  }
+)
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
